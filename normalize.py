91bb3d32d930e00d54a99562f300ecc1e676755b
Spisocheg <spisocheg@users.noreply.github.com>
2026-09-14 01:34:07 +0300

    +normalize module (best formating)

--- normalize.py
+++ normalize.py
@@ -9,14 +9,10 @@ def _extract_raw_events(responses: list) -> list[_Element]:
     not_filtered_events = []
     for resp in responses:
         not_filtered_events.extend(resp.xpath('//results/item'))
-    # not_filtered_events = [*resp.xpath('//results/item') for resp in responses]
 
     raw_events = list(filter(
         lambda ev: ev.xpath('./eventType')[0].text == 'event', not_filtered_events
     ))
-    # for ev in not_filtered_events:
-    #     if ev.xpath('//eventType')[0].text == 'event':
-    #         raw_events.append(ev)
 
     return raw_events
 
@@ -36,7 +32,7 @@ def _transform_events2model(raw_events: list[_Element]) -> list[Event]:
                         id_=raw_e.xpath('./courseId')[0].text.strip('{}'),
                         name=raw_e.xpath('./courseName')[0].text
                     )
-                )
+                )   # str_type заполняется до валидации декоратором модели
             )
         except AttributeError:
             pass
@@ -53,15 +49,7 @@ def _dedup_events(events: list[Event]) -> list[Event]:
     return filtered_e
 
 
-def normalize(responses: list[_Element]) -> set[Event]:
-    # проходит итеративно по списку респонсов
-    #   парсить хмл
-    #   искать требуемые данные
-    #   мб валидировать
-
+def normalize(responses: list[_Element]) -> list[Event]:
     raw_events = _extract_raw_events(responses)
     events = _transform_events2model(raw_events)
-    events = _dedup_events(events)
-
-
-
+    return _dedup_events(events)
